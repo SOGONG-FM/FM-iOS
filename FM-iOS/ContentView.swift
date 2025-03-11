1ef11236708465563902fefd9b19727c89229518
Choyoungjun <cyj070513@gmail.com>
2025-03-11 15:17:36 +0900

    Initial Commit

---
+++ FM-iOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FM-iOS
+//
+//  Created by 조영준 on 3/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FM-iOS/FM_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  FM_iOSApp.swift
+//  FM-iOS
+//
+//  Created by 조영준 on 3/11/25.
+//
+
+import SwiftUI
+
+@main
+struct FM_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
